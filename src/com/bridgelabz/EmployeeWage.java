b0eab118ed0c6bccd056fe66a70396a6c9d24832
Kiran1695 <hindlekarkiran.kh@gmail.com>
2023-01-06 17:04:02 +0530

    Woring Hours Or Days

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,12 +6,14 @@ public class EmployeeWage {
     static final int FULL_DAY_PER_HOUR = 8;
     static final int PART_TIME_HOUR = 4;
     static final int WORKING_DAYS_PER_MONTH = 20;
+    static final int WORKING_HOURS_PER_MONTH = 100;
 
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Problem");
         int dayCount = 1;
-        while (dayCount <= WORKING_DAYS_PER_MONTH) {
+        int workingHrs = 0;
+        while (dayCount <= WORKING_DAYS_PER_MONTH && workingHrs < WORKING_HOURS_PER_MONTH) {
             int employeeCheck = (int) (Math.random() * 10) % 2;
             System.out.println(employeeCheck);
             int dailyWage = 0;
@@ -19,15 +21,18 @@ public class EmployeeWage {
                 case 1:
                     System.out.println("Employee is Full Time");
                     dailyWage = WAGE_PER_HOUR * FULL_DAY_PER_HOUR;
+                    workingHrs = workingHrs + FULL_DAY_PER_HOUR;
                     break;
                 case 2:
                     System.out.println("Employee is Part Time");
                     dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    workingHrs = workingHrs + PART_TIME_HOUR;
                     break;
                 default:
                     System.out.println("Employee is Absent");
             }
             System.out.println("Daily Wage of Day => " + dayCount + " is " + dailyWage);
+            System.out.println("working Hrs => " + workingHrs);
             dayCount++;
         }
     }
